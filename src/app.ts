f371bef83fd141818c576687d7670b40907104d6
Pasindu Dilshan <Pasindu_dilshan@outlook.com>
2021-06-19 20:08:55 +0530

    Create a user with prisma client. Prettier update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 class App {
-    public main(): void {
-        console.log("Application Logic Goes Here");
+    public async main(): Promise<void> {
+        const result = await prisma.user.create({
+            data: { username: "Harper Lee", email: "Harper@lee.com" },
+        });
+
+        console.log(`Query Result : ${JSON.stringify(result)}`);
     }
 }
 
